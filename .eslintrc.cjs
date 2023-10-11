0ea6b5091960477e130389ba40bd8e806864b62f
newExpand <magin0auto@gmail.com>
2023-10-11 18:58:41 +0900

    :tada:start: 리액트 초기세팅 추가

---
+++ .eslintrc.cjs
@@ -0,0 +1,31 @@
+module.exports = {
+    root: true,
+    env: { browser: true, es6: true },
+    extends: [
+        'plugin:prettier/recommended',
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:react/recommended',
+        'plugin:import/recommended',
+        'plugin:react-hooks/recommended',
+        'plugin:@react-three/recommended',
+    ],
+    ignorePatterns: ['dist', '.eslintrc.cjs'],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaFeatures: { jsx: true },
+        ecmaVersion: 2018,
+        sourceType: 'module',
+    },
+    plugins: ['react-refresh'],
+    rules: {
+        'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+        'react/react-in-jsx-scope': 'off',
+        'no-console': ['warn', { allow: ['warn', 'error'] }],
+        'import/export': 'error',
+        '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+        'react/no-unknown-property': 'off',
+        '@react-three/no-clone-in-loop': 'warn',
+        '@react-three/no-new-in-loop': 'warn',
+    },
+};
