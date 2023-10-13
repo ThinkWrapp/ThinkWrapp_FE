b8dbb124d09d96e01035935eeedda3ad00839630
newExpand <magin0auto@gmail.com>
2023-10-13 18:37:26 +0900

    :heavy_plus_sign:chore: 리액트 쿼리 및 데브툴 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Lobby from './pages/Lobby';
 import RootLayOut from './components/Layout/RootLayOut';
 import LoadAuth from './components/SocialLogin/LoadAuth';
+
+const queryClient = new QueryClient();
+
 const router = createBrowserRouter([
     {
         path: '/',
@@ -14,7 +19,12 @@ const router = createBrowserRouter([
 ]);
 
 const App = () => {
-    return <RouterProvider router={router} />;
+    return (
+        <QueryClientProvider client={queryClient}>
+            <RouterProvider router={router} />
+            <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
+    );
 };
 
 export default App;
