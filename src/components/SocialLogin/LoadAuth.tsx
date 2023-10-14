d83b40ca75cc5721b0eac803cf010bb990ac5594
newExpand <magin0auto@gmail.com>
2023-10-15 03:09:45 +0900

    :sparkles:feat: 구글 소셜 로그인에 쿠키를 통해 토큰 저장 컴포넌트 작성

--- src/components/SocialLogin/LoadAuth.tsx
+++ src/components/SocialLogin/LoadAuth.tsx
@@ -1,3 +1,25 @@
+import { useEffect } from 'react';
+import { userStorage } from '../../utils/userStorage';
+import { refreshToken } from '../../api/auth';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'sonner';
+
 export default function LoadAuth() {
-    return <div>LoadAuth</div>;
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        try {
+            refreshToken().then((response) => {
+                userStorage.set(response.access_token);
+                toast.success('소셜로그인에 성공했습니다.');
+            });
+        } catch (error) {
+            toast.error('소셜로그인에 실패했습니다.');
+            console.error(error);
+        } finally {
+            navigate('/');
+        }
+    }, []);
+
+    return null;
 }
