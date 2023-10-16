a0d8ede1126265e431cd09b3277ebfbbc589048c
newExpand <magin0auto@gmail.com>
2023-10-17 06:25:58 +0900

    :lipstick:design: 스타일 background-color 테마 추가

--- src/theme.ts
+++ src/theme.ts
@@ -1,16 +1,35 @@
-export default {
+const theme = {
     fs: {
         sm: '1.2rem',
         md: '1.6rem',
         lg: '2rem',
-    },
+    } as const,
     fc: {
-        black: '#000',
-        white: '#fff',
-    },
+        dark: '#212529',
+        outDark: '#343a40',
+        light: '#fff',
+        outLight: '#f8f9fa',
+        primary: '#007bff',
+        secondary: '#6c757d',
+        success: '#28a745',
+        danger: '#dc3545',
+    } as const,
     fw: {
         thin: 200,
         normal: 400,
         bold: 600,
-    },
-};
+    } as const,
+    bg: {
+        dark: '#343a40',
+        light: '#f8f9fa',
+        primary: '#007bff',
+        secondary: '#6c757d',
+        success: '#28a745',
+        danger: '#dc3545',
+        out: 'transparent',
+    } as const,
+} as const;
+
+export default theme;
+
+export type ThemeType = typeof theme;
