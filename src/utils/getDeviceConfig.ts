4fb52c9df0a589d51fb7085db7d7a841321eebab
newExpand <magin0auto@gmail.com>
2023-10-23 22:02:24 +0900

    :recycle:refactor: 모바일 반응형 시 태블릿 반응형에서 수정 사항 없을 때에 대한 옵션 추가

--- src/utils/getDeviceConfig.ts
+++ src/utils/getDeviceConfig.ts
@@ -1,3 +1,6 @@
-export const getDeviceConfig = (device: string, config: Record<string, any>) => {
+export const getDeviceConfig = (device: string, config: Record<string, any>, useTabletAsDefaultForMobile = false) => {
+    if (device === 'mobile' && useTabletAsDefaultForMobile) {
+        return config.tablet;
+    }
     return device in config ? config[device] : config.desktop;
 };
