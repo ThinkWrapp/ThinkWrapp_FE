d3d3e547d0d33fc9ca68153824fa4cde57fe0e37
newExpand <magin0auto@gmail.com>
2023-10-25 18:49:37 +0900

    :bug:fix: theme styled.d.ts 추가 및 타입스크립트 인텔리센스 과부하로 인한 버전 다운그레이드로 인한 타입에러 ignore 처리

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import ReactPortalDom from 'react-dom';
-import App from './App.tsx';
+import App from './App';
 import './index.css';
 
 import { Provider } from 'react-redux';
@@ -9,9 +9,9 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { Toaster } from 'sonner';
 import { ThemeProvider } from 'styled-components';
-import theme from './theme.ts';
-import store, { persistor } from './store.ts';
+import store, { persistor } from './store';
 import { PersistGate } from 'redux-persist/integration/react';
+import theme from './theme';
 
 const queryClient = new QueryClient();
 
@@ -20,6 +20,7 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
         <Provider store={store}>
             <PersistGate loading={null} persistor={persistor}>
                 <QueryClientProvider client={queryClient}>
+                    {/* @ts-ignore */}
                     <ThemeProvider theme={theme}>
                         <App />
                     </ThemeProvider>
