c6bfad9af4e846600d274c5f8a6e36681c9f7c3c
newExpand <magin0auto@gmail.com>
2023-11-08 03:17:59 +0900

    :sparkles:feat: env 변수 추가

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -2,4 +2,8 @@
 
 interface ImportMetaEnv {
     readonly VITE_SEVER_BASE_URL: string;
+    readonly VITE_PEER_PORT: number;
+    readonly VITE_PEER_PATH: string;
+    readonly VITE_PEER_HOST: string;
+    readonly VITE_SOCKTE_URL: string;
 }
